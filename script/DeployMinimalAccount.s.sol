85dca4d8c969a1f0fef01a9c69659c9bc13a94bf
anselmo <anselmostitla@gmail.com>
2025-06-08 11:45:48 -0600

    updating transfer ownership

--- script/DeployMinimalAccount.s.sol
+++ script/DeployMinimalAccount.s.sol
@@ -17,7 +17,7 @@ contract DeployMinimalAccount is Script {
 
       vm.startBroadcast(config.account);
       MinimalAccount minimalAccount = new MinimalAccount(config.entryPoint);
-      minimalAccount.transferOwnership(msg.sender);
+      minimalAccount.transferOwnership(config.account);
       vm.stopBroadcast();
 
       return (helperConfig, minimalAccount);
